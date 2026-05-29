9eb34fa307380652131a04c22906892f82894c73
Dyxa1234 <sharypov2007andrei@bk.ru>
2026-05-30 01:17:11 +0700

    Добавил lab_17

---
+++ lab_18/paint++/src/CMakeFiles/paint.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paint.dir/main.cpp.o"
+  "CMakeFiles/paint.dir/main.cpp.o.d"
+  "CMakeFiles/paint.dir/paint_core.cpp.o"
+  "CMakeFiles/paint.dir/paint_core.cpp.o.d"
+  "paint"
+  "paint.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/paint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
